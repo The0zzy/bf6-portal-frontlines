4c0e5e3a7474e584c49a7e5ccf7df18b11177c51
Eric Wünsche <wuensche.e@gmail.com>
2026-06-29 14:23:07 +0200

    getting started

--- src/Frontlines/Frontlines.ts
+++ src/Frontlines/Frontlines.ts
@@ -15,22 +15,87 @@ export class Frontlines {
     mcoms: mod.MCOM[] = [];
     eventSubscriptions: (() => void)[] = [];
 
+
     static init() {
         if (Frontlines.instance) {
             return;
         }
         Frontlines.instance = new Frontlines();
-        Frontlines.instance.eventSubscriptions.push(
-            Events.OnGameModeStarted.subscribe(
-                async () => {
-                    console.log("Frontlines.init() called");
-                }
+        Frontlines.instance.init();
+    }
+
+    init() {
+        // Disable all capture points at the start of the game
+        const allCapturePoints = mod.AllCapturePoints();
+        for (let i = 0; i < mod.CountOf(allCapturePoints); i++) {
+            const capturePoint = mod.ValueInArray(allCapturePoints, i);
+            mod.EnableGameModeObjective(capturePoint, false);
+        }
+
+        // Frontlines capture point object ID range: 0-9
+        for (let i = 0; i < 10; i++) {
+            const capturePoint = mod.GetCapturePoint(i);
+            if (capturePoint) {
+                this.captureChain.push(capturePoint);
+            }
+        }
+
+        // set the initial capture chain index to the middle of the chain
+        this.captureChainIndex = Math.floor(this.captureChain.length / 2);
+
+        this.updateCapturePoints();
+
+        this.subscribeToEvents();
+    }
+
+    updateCapturePoints() {
+        // Update the capture points based on the current capture chain index
+        for (let i = 0; i < this.captureChain.length; i++) {
+            const capturePoint = this.captureChain[i];
+            mod.EnableGameModeObjective(capturePoint, i === this.captureChainIndex);
+            mod.EnableCapturePointDeploying(capturePoint, false);
+            mod.SetCapturePointCapturingTime(capturePoint, this.captureTime);
+            mod.SetMaxCaptureMultiplier(capturePoint, 1.0);
+            mod.SetCapturePointNeutralizationTime(capturePoint, this.neutralizationTime);
+        }
+    }
+
+
+    onCapturePointCaptured(capturePoint: mod.CapturePoint) {
+        if (mod.GetCurrentOwnerTeam(capturePoint) === mod.GetTeam(1)) {
+            if (this.captureChainIndex == this.captureChain.length - 1) {
+                // Team 1 has captured the last capture point, start mcom capture sequence
+                this.startMcomCaptureSequence(mod.GetTeam(1));
+            } else {
+                this.captureChainIndex++;
+            }
+        } else {
+            if (this.captureChainIndex == 0) {
+                // Team 2 has captured the first capture point, start mcom capture sequence
+                this.startMcomCaptureSequence(mod.GetTeam(2));
+            } else {
+                this.captureChainIndex--;
+            }
+        }
+    }
+
+    startMcomCaptureSequence(attackerTeam: mod.Team) {
+        mod.SendErrorReport(mod.Message(mod.stringkeys.frontlines.debug.mcom_phase));
+    }
+
+    subscribeToEvents() {
+        this.eventSubscriptions.push(
+            Events.OnCapturePointCaptured.subscribe(
+                (capturePoint: mod.CapturePoint) => this.onCapturePointCaptured(capturePoint)
             )
         );
     }
 
-    destroy() {
-        this.eventSubscriptions.forEach(unsubscribe => unsubscribe());
-        this.eventSubscriptions = [];
+    static destroy() {
+        if (Frontlines.instance) {
+            Frontlines.instance.eventSubscriptions.forEach(unsubscribe => unsubscribe());
+            Frontlines.instance.eventSubscriptions = [];
+            Frontlines.instance = null;
+        }
     }
 }
